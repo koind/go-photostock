673cd0fc1f862f462695cbbe77bcce128d546b51
Damir15 <krosava_15d@mail.ru>
2019-02-23 23:32:24 +0600

    Добавил временную логику в контроллер

--- main.go
+++ main.go
@@ -5,14 +5,41 @@ import (
 	"log"
 	"github.com/gorilla/mux"
 	"encoding/json"
+	"html/template"
+	"fmt"
+	"io/ioutil"
 )
 
 type ImageController struct {
 }
 
+func (imgC *ImageController) indexAction(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("template/index.html"))
+	tmpl.Execute(w, nil)
+}
+
 func (imgC *ImageController) newAction(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	json.NewEncoder(w).Encode(params)
+	file, info, err := r.FormFile("image")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	contentType := info.Header.Get("Content-Type")
+	if !(contentType == "image/png" || contentType == "image/jpeg" || contentType == "image/gif") {
+		fmt.Printf("Wrong content type: %s", contentType)
+		return
+	}
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//imageName := fmt.Sprintf("%d.png", time.Now().Unix())
+
+	ioutil.WriteFile(info.Filename, data, 0600)
 }
 
 func (imgC *ImageController) getOneByIdAction(w http.ResponseWriter, r *http.Request) {
@@ -27,8 +54,8 @@ func (imgC *ImageController) getOneByIdAndPartIdAction(w http.ResponseWriter, r 
 
 func main() {
 	imageController := ImageController{}
-
 	router := mux.NewRouter()
+	router.HandleFunc("/", imageController.indexAction).Methods("GET")
 	router.HandleFunc("/images", imageController.newAction).Methods("POST")
 	router.HandleFunc("/images/{id}", imageController.getOneByIdAction).Methods("GET")
 	router.HandleFunc("/images/{id}/parts/{part_num}", imageController.getOneByIdAndPartIdAction).Methods("GET")
